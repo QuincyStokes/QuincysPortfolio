aeff9e8abe406150af30326c78d3bdafeeabb3aa
qstokes <quincy.f.stokes@gmail.com>
2025-07-05 11:30:40 -0700

    No more Vite, Render instead

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const expressLayouts = require('express-ejs-layouts');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Listening on port ${PORT}`)
+);
+
 
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
